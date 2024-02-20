f4b5d5bc953584e3ac7d056bd3203491f51cbde7
davidmikulic03 <d.mikulic03@gmail.com>
2024-02-20 15:38:37 +0100

    Main Tweak #2
    I need to rework the conversion from orbital elements to the state vectors. They are very inaccurate.

--- Source/Source.cpp
+++ Source/Source.cpp
@@ -2,24 +2,31 @@
 #include <iostream>
 #include <exception>
 #include <chrono>
+#include <exception>
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv) {
 	using namespace Eigen;
 
-	Vector3 pos = Vector3(500, 0, 0);
-	Vector3 vel = Vector3(0.0, 2, 1);
+	try {
+		Vector3 pos = Vector3(500, 0, 0);
+		Vector3 vel = Vector3(0.0, 2.0, 0.8);
 
-	Physics::Orbit orbit = Physics::Orbit::FromStateVectors(3e13, pos, vel);
-	double dt = 10;
-	double time = 0;
-	for (unsigned int i = 0; i < 1000; i++) {
-		orbit.EvaluateTrueAnomaly(time, 10);
-		Physics::Orbit::ToLocalStateVectors(orbit, pos, vel);
-		std::cout << pos.Magnitude() << std::endl;
-		time += dt;
+		Physics::Orbit orbit = Physics::Orbit::FromStateVectors(3e13, pos, vel);
+		double dt = 10;
+		double time = 0;
+		for (unsigned int i = 0; i < 1000; i++) {
+			orbit.EvaluateTrueAnomaly(time, 10);
+			Physics::Orbit::ToLocalStateVectors(orbit, pos, vel);
+			std::cout << pos.Magnitude() << std::endl;
+			time += dt;
+		}
+
+		std::cout << "Periapsis: " << orbit.Periapsis() << ", Apoapsis: " << orbit.Apoapsis() << ", Eccentricity: " << orbit.eccentricity << ", Orbital Period: " << orbit.Period() << std::endl;
 	}
-
-	std::cout << "Periapsis: " << orbit.Periapsis() << ", Apoapsis: " << orbit.Apoapsis() << ", Eccentricity: " << orbit.eccentricity << ", Orbital Period: " << orbit.Period() << std::endl;
+	catch (std::exception e) {
+		std::cerr << e.what() << std::endl;
+		return -1;
+	}
 
 	return 0;
 }
