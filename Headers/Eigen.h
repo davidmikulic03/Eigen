7bf0c86635ace3755c107d07104b7fa607c96403
davidmikulic03 <d.mikulic03@gmail.com>
2024-02-20 15:17:16 +0100

    Main parameter tweak

--- Headers/Eigen.h
+++ Headers/Eigen.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <cmath>
-#include <iostream>
 #include "Constants.h"
 #include "Vector3.h"
 #include "Quaternion.h"
--- Source/Source.cpp
+++ Source/Source.cpp
@@ -6,11 +6,11 @@
 int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv) {
 	using namespace Eigen;
 
-	Vector3 pos = Vector3(10000, 0, 0);
-	Vector3 vel = Vector3(5.0, 1000.0, -500.0);
+	Vector3 pos = Vector3(1000, 0, 0);
+	Vector3 vel = Vector3(0.0, 1, 0.5);
 
-	Physics::Orbit orbit = Physics::Orbit::FromStateVectors(Physics::EarthMass * 0.0001, pos, vel);
-	double dt = .1;
+	Physics::Orbit orbit = Physics::Orbit::FromStateVectors(2e13, pos, vel);
+	double dt = 5;
 	double time = 0;
 	for (unsigned int i = 0; i < 1000; i++) {
 		orbit.EvaluateTrueAnomaly(time, 10);
